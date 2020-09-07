53901f155cb121d0ef0c173eeaf89e77359f9c00
Aniket Dinda <38289183+deadlysnowman3308@users.noreply.github.com>
2020-09-07 16:50:06 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,29 +1,20 @@
-# Import QRCode from pyqrcode 
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import pyqrcode
 import os
 from pyqrcode import QRCode
-
 user = input('Enter your text>>  ')
 user = str(user)
-
-
-
-# String which represent the QR code 
 s = user
-
-# Generate QR code 
-url = pyqrcode.create(s) 
-
-# Create and save the png file naming "myqr.png" 
-url.svg("myqr.svg", scale = 9) 
-
-
-print()
-print()
-print()
-print("Your qrcode text is >>>>")
-print(user)
-print()
-print()
-print()
-print()
+url = pyqrcode.create(s)
+url.svg('myqr.svg', scale=9)
+print ()
+print ()
+print ()
+print 'Your qrcode text is >>>>'
+print user
+print ()
+print ()
+print ()
+print ()
